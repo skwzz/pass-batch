2744686ce506c73086f99e83e0a87a7651ab3668
unknown <kidol9108@gmail.com>
2023-12-04 23:15:13 +0900

    first commit

---
+++ src/test/java/com/skwzz/passbatch/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwzz.passbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
